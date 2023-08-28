dbc3f7870afc406ece19d4ef3887038b7bb032d6
Jhonathan Nila <nilalimon@gmail.com>
2023-08-28 09:52:04 -0600

    POO, se crea primer clase

---
+++ 01.php
@@ -0,0 +1,33 @@
+<?php 
+    declare(strict_types = 1);
+
+include 'includes/header.php';
+
+    // Definir una clase
+    class Producto {
+        public $nombre;
+        public $precio;
+        public $disponible;
+
+        // Creando métodos o funciones dentro de la clase
+        public function __construct(string $nombre, int $precio, bool $disponible)
+        {
+            $this -> nombre = $nombre;
+            $this -> precio = $precio;
+            $this -> disponible = $disponible;
+        }
+    }
+
+    $producto = new Producto('Tablet', 200, true);
+
+    echo "<pre>";
+    var_dump($producto);
+    echo "</pre>";
+
+    $producto2 = new Producto('Monitor Curvo', 300, true);
+
+    echo "<pre>";
+    var_dump($producto2);
+    echo "</pre>";
+    
+include 'includes/footer.php';
