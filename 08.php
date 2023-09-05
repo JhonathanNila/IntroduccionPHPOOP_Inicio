05cd2453cb5863152cfa620eb66ed63d5c0b169e
Jhonathan Nila <nilalimon@gmail.com>
2023-09-05 00:14:03 -0600

    Se agregan namespaces

--- 08.php
+++ 08.php
@@ -1,10 +1,16 @@
 <?php include 'includes/header.php';
 
+// require 'clases/Clientes.php';
+// require 'clases/Detalles.php';
+
+use App\Clientes;
+use App\Detalles;
+
 // Incluir las otras clases
 function mi_autoload($clase) {
-    require __DIR__ . '/clases/' . $clase . '.php';
+    $partes = explode('\\', $clase);
+    require __DIR__ . '/clases/' . $partes[1] . '.php';
 }
-
 spl_autoload_register('mi_autoload');
 
 $detalles = new Detalles();
