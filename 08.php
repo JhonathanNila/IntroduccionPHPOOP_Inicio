22e4bb67c3601e347989c90a9f436109ef0881bf
Jhonathan Nila <nilalimon@gmail.com>
2023-09-04 23:37:15 -0600

    Se agrega Autoload

--- 08.php
+++ 08.php
@@ -1,4 +1,13 @@
 <?php include 'includes/header.php';
 
+// Incluir las otras clases
+function mi_autoload($clase) {
+    require __DIR__ . '/clases/' . $clase . '.php';
+}
+
+spl_autoload_register('mi_autoload');
+
+$detalles = new Detalles();
+$clientes = new Clientes();
 
 include 'includes/footer.php';
