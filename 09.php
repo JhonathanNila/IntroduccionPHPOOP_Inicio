4e78bfa8ca2aba7afd22f88a3f07d6e255055926
Jhonathan Nila <nilalimon@gmail.com>
2023-09-09 18:09:35 -0600

    Se consultado DB con POO y mysqli

--- 09.php
+++ 09.php
@@ -1,4 +1,36 @@
 <?php include 'includes/header.php';
 
+// Conectar a la DB con mysqli 
+
+$db = new mysqli('localhost', 'root', '', 'bienesraices_crud');
+
+// Creamos el query
+$query = "SELECT titulo, imagen FROM propiedades";
+
+// Lo preparamos
+$stmt = $db -> prepare($query);
+
+// Lo ejecutamos
+$stmt -> execute();
+
+// Creamos la variable
+$stmt -> bind_result($titulo, $imagen);
+
+// Asignamos el resultado
+$stmt -> fetch();
+
+// Imprimir el resultado
+var_dump($titulo);
+var_dump($imagen);
+
+
+
+// Multiples resultados
+// while($row = $resultado -> fetch_assoc()) : 
+//     var_dump($row);
+// endwhile;
+
+// Un solo resultado
+// var_dump($resultado -> fetch_assoc());
 
 include 'includes/footer.php';
